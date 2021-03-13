74ffca71a7e86f223b1a9b61e79d03849990a02d
Thibault-Goedert <thibault.goedert@student.yncrea.com>
2021-03-13 16:58:48 +0100

    pb scene builder

--- src/main/resources/isen/view/MainPageController.java
+++ src/main/resources/isen/view/MainPageController.java
@@ -1,4 +1,44 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import App.java;
+import Person.java;
+
 
 public class MainPageController {
+ObservableList<E> list=FXCollections.observableArrayList();
+	
+	@FXML
+    private ListView<String> repertoireList;
+	
+	private ListViewItem contact;
+	
+	public void initialize(URL url, ResourceBundle rb) {
+		loadData();
+	}
 
+	private void loadData() {
+		list.removeAll(list);
+		String a="mon premier contact";
+		String b="Mon deuxieme contact";
+		String c="Mon troisieme contact";
+		String d="Mon quatrieme contact";
+		list.addAll(ab,c,d);
+		repertoireList.getItems().addAll(list);
+	}
+	
+	@FXML
+	private void DisplaySelected(MouseEvent event) {
+		App.setRoot("/isen/view/DetailContact");
+	}
+	
+	@FXML
+    private Button addButton;
+
+    @FXML
+    void addContact(ActionEvent event) {
+    	//addPerson()
+    	App.setRoot("/isen/view/Formulaire");
+    }
 }
